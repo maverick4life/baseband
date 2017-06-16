27df1bf06aef39278da2466e666ff1a7c89313d3
maverick4life <mk@maverickinfosec.com>
2017-06-15 18:57:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 sudo apt-get -y update
-cp .screenrc ~/
+sudo apt-get -y install screen vim
+cp "$parent_path/.screenrc" ~/
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-git clone https://github.com/leebaird/discover ~/discover/
+
